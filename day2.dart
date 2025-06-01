1d42f19fec07bd214072c0e15928925932df68fc
tutus <74859534+amrahsali@users.noreply.github.com>
2025-06-01 16:01:43 +0100

    Update day2.dart

--- day2.dart
+++ day2.dart
@@ -10,3 +10,26 @@ void main() {
   var solution = formular(10, 40);
   print(solution);
 }
+
+
+
+//Create a function that accepts 2 numbers and returns the greater.
+  
+  
+  int greater(int num1, int num2 ){
+  
+    
+  if(num1 > num2){
+    return num1;
+  }else{
+     return num2;
+  }
+  
+   
+}
+void main() {
+
+int result = greater(10, 5);
+  
+  print(result);
+}
