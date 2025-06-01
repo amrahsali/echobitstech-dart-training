eda35629ab13b0f3ddab0ed8a7c831e02ed9e948
tutus <74859534+amrahsali@users.noreply.github.com>
2025-06-01 13:54:54 +0100

    Create day2.dart

---
+++ day2.dart
@@ -0,0 +1,12 @@
+// Do a calculation of area of a rectangle using two variables.
+  
+double formular(int weight, int height) {
+  double mbi = weight / height * height;
+
+  return mbi;
+}
+
+void main() {
+  var solution = formular(10, 40);
+  print(solution);
+}
